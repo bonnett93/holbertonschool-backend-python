76d52a86c43db604283867034ba58b4e7179a46b
bonnett93 <bonnettjordan@gmail.com>
2022-04-21 19:22:23 -0500

    Upload tasks 0-4

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+2-floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    type-annotated function which takes a float n as argument
+    and returns the floor of the float.
+    """
+    return int(n)
