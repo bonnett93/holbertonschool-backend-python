fa18630fa6d9b0aef37d73c1a0d80ec69003b03b
bonnett93 <bonnettjordan@gmail.com>
2022-04-26 14:25:07 -0500

    Fix pycode style in file 0-async...

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,8 +2,10 @@
 """
 0-async_generator
 """
-import asyncio, random
+import asyncio
+import random
 from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
     """
@@ -12,4 +14,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
