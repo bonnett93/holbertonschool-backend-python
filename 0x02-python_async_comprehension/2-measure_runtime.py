bc5b0932cae0451a4664ecabca725c045665d827
bonnett93 <bonnettjordan@gmail.com>
2022-04-26 15:56:11 -0500

    Add task list

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,11 +13,8 @@ async def measure_runtime() -> float:
     t will execute async_comprehension four times in parallel
     using asyncio.gather. measure the total runtime and return it.
     """
-    s = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
-    total_time = time.time() - s
+    s = time.perf_counter()
+    task_list = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*task_list)
+    total_time = time.perf_counter() - s
     return total_time
